b55d2a151a42d57fe620fe431647df8bd4cc0423
bweick <brian.weickmann@gmail.com>
2021-02-02 09:51:29 -0800

    Final params for Feb rebalance.

--- index-rebalances/assetInfo.ts
+++ index-rebalances/assetInfo.ts
@@ -17,50 +17,50 @@ export interface Assets {
 export const assets: Assets = {
   YFI: {
     address: "0x0bc529c00C6401aEF6D220BE8C6Ea1667F6Ad93e",
-    price: ether(30207.44) ,
+    price: ether(30516.87) ,
   },
   COMP: {
     address: "0xc00e94Cb662C3520282E6f5717214004A7f26888",
-    price: ether(370.85),
+    price: ether(351.55),
   },
   SNX: {
     address: "0xC011a73ee8576Fb46F5E1c5751cA3B9Fe0af2a6F",
-    price: ether(17.22),
+    price: ether(17.19),
   },
   MKR: {
     address: "0x9f8F72aA9304c8B593d555F12eF6589cC3A579A2",
-    price: ether(1461.61),
+    price: ether(1473.40),
   },
   REN: {
     address: "0x408e41876cCCDC0F92210600ef50372656052a38",
-    price: ether(0.6044),
+    price: ether(0.6043),
   },
   KNC: {
     address: "0xdd974D5C2e2928deA5F71b9825b8b646686BD200",
-    price: ether(1.31),
+    price: ether(1.36),
   },
   LRC: {
     address: "0xBBbbCA6A901c926F240b89EacB641d8Aec7AEafD",
-    price: ether(0.4880),
+    price: ether(0.5106),
   },
   BAL: {
     address: "0xba100000625a3754423978a60c9317c58a424e3D",
-    price: ether(26.40),
+    price: ether(28.93),
   },
   UNI: {
     address: "0x1f9840a85d5aF5bf1D1762F925BDADdC4201F984",
-    price: ether(18.31),
+    price: ether(20.48),
   },
   AAVE: {
     address: "0x7Fc66500c84A76Ad7e9c93437bFc5Ac33E2DDaE9",
-    price: ether(301.39),
+    price: ether(303.41),
   },
   MTA: {
     address: "0xa3BeD4E1c75D00fa6f4E5E6922DB7261B5E9AcD2",
-    price: ether(2.97),
+    price: ether(3.47),
   },
   WETH: {
     address: "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2",
-    price: ether(1312.88),
+    price: ether(1330.80),
   }
 };
