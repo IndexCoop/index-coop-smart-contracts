d4f21db024f07cd197208a8ba8087de513530825
bweick <brian.weickmann@gmail.com>
2021-02-20 18:29:36 -0800

    Bringing in Uniswap set up and contracts.

---
+++ utils/contracts/uniswap.ts
@@ -0,0 +1,4 @@
+// External Uniswap Contracts
+export { UniswapV2Factory } from "../../typechain/UniswapV2Factory";
+export { UniswapV2Pair } from "../../typechain/UniswapV2Pair";
+export { UniswapV2Router02 } from "../../typechain/UniswapV2Router02";
---
+++ utils/fixtures/uniswapFixture.ts
@@ -0,0 +1,49 @@
+import DeployHelper from "../deploys";
+import { Signer } from "ethers";
+import { JsonRpcProvider, Web3Provider } from "@ethersproject/providers";
+import { Address } from "../types";
+
+import {
+  UniswapV2Factory,
+  UniswapV2Pair,
+  UniswapV2Router02
+} from "../contracts/uniswap";
+import { UniswapV2Pair__factory } from "../../typechain/factories/UniswapV2Pair__factory";
+
+export class UniswapFixture {
+  private _deployer: DeployHelper;
+  private _ownerSigner: Signer;
+
+  public owner: Address;
+  public factory: UniswapV2Factory;
+  public pair: UniswapV2Pair;
+  public router: UniswapV2Router02;
+
+  public wethDaiPool: UniswapV2Pair;
+  public wethWbtcPool: UniswapV2Pair;
+  public uniWethPool: UniswapV2Pair;
+
+  constructor(provider: Web3Provider | JsonRpcProvider, ownerAddress: Address) {
+    this._ownerSigner = provider.getSigner(ownerAddress);
+    this._deployer = new DeployHelper(this._ownerSigner);
+  }
+
+  public async initialize(_owner: Address, _weth: Address, _wbtc: Address, _usdc: Address): Promise<void> {
+    this.owner = _owner;
+    this.factory = await this._deployer.external.deployUniswapV2Factory(this.owner);
+    this.router = await this._deployer.external.deployUniswapV2Router02(this.factory.address, _weth);
+
+    this.wethDaiPool = await this.createNewPair(_weth, _usdc);
+    this.wethWbtcPool = await this.createNewPair(_weth, _wbtc);
+  }
+
+  public async createNewPair(_tokenOne: Address, _tokenTwo: Address): Promise<UniswapV2Pair> {
+    await this.factory.createPair(_tokenOne, _tokenTwo);
+    const poolAddress = await this.factory.allPairs((await this.factory.allPairsLength()).sub(1));
+    return await new UniswapV2Pair__factory(this._ownerSigner).attach(poolAddress);
+  }
+
+  public getTokenOrder(_tokenOne: Address, _tokenTwo: Address): [Address, Address] {
+    return _tokenOne.toLowerCase() < _tokenTwo.toLowerCase() ? [_tokenOne, _tokenTwo] : [_tokenTwo, _tokenOne];
+  }
+}
