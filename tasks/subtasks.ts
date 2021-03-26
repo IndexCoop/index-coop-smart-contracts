7fa220afeb7ecff4ab0894b0ac0043ff398b0a50
cgewecke <christophergewecke@gmail.com>
2021-03-26 15:01:02 -0700

    Inject gas setting into ABIs and move tasks from hardhat.config to tasks (#33)

---
+++ tasks/subtasks.ts
@@ -0,0 +1,23 @@
+import {
+  TASK_COMPILE_SOLIDITY_COMPILE,
+  TASK_TEST_SETUP_TEST_ENVIRONMENT,
+  TASK_COMPILE_SOLIDITY_GET_ARTIFACT_FROM_COMPILATION_OUTPUT,
+} from "hardhat/builtin-tasks/task-names";
+
+import { subtask, internalTask } from "hardhat/config";
+import { addGasToAbiMethods, setupNativeSolc } from "../utils/tasks";
+
+// Injects network block limit (minus 1 million) in the abi so
+// ethers uses it instead of running gas estimation.
+subtask(TASK_COMPILE_SOLIDITY_GET_ARTIFACT_FROM_COMPILATION_OUTPUT)
+  .setAction(async (_, { network }, runSuper) => {
+    const artifact = await runSuper();
+    artifact.abi = addGasToAbiMethods(network.config, artifact.abi);
+    return artifact;
+  }
+);
+
+// Use native solc if available locally at config specified version
+internalTask(TASK_COMPILE_SOLIDITY_COMPILE).setAction(setupNativeSolc);
+
+export {};
