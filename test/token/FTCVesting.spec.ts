82b345719843d4d54fcd0674f2d4ea1d7c078f49
Zachary Parrish <zachary@parrish.io>
2021-06-02 21:28:31 -0400

    completed unit testing

--- test/token/FTCVesting.spec.ts
+++ test/token/FTCVesting.spec.ts
@@ -74,7 +74,7 @@ describe("FTCVesting", () => {
     });
   });
 
-  xdescribe("#setRecipient", async () => {
+  describe("#setRecipient", async () => {
     let subjectVestingAmount: BigNumber;
     let subjectVestingStart: BigNumber;
     let subjectVestingCliff: BigNumber;
@@ -105,16 +105,60 @@ describe("FTCVesting", () => {
       return subjectFtcVesting.connect(recipient.wallet).setRecipient(newRecipient.address);
     }
 
-    it("should set new recipient", async () => {
+    it("should set new recipient address", async () => {
       const currentRecipientAddress = await subjectFtcVesting.recipient();
 
       expect(currentRecipientAddress).to.eq(recipient.address);
 
       await subject();
 
-      const newRecipientAddress = subjectFtcVesting.recipient();
+      const newRecipientAddress = await subjectFtcVesting.recipient();
 
       expect(newRecipientAddress).to.eq(newRecipient.address);
+    });
+  });
+
+  describe("#setTreasury", async () => {
+    let subjectVestingAmount: BigNumber;
+    let subjectVestingStart: BigNumber;
+    let subjectVestingCliff: BigNumber;
+    let subjectVestingEnd: BigNumber;
+    let subjectFtcVesting: FTCVesting;
+    let newTreasury: Account;
+
+    beforeEach(async () => {
+      const now = await getLastBlockTimestamp();
+      newTreasury = await getRandomAccount();
+      subjectVestingStart = now;
+      subjectVestingCliff = now.add(60 * 60 * 24 * 183);
+      subjectVestingEnd = now.add(60 * 60 * 24 * 547);
+      subjectVestingAmount = ether(100);
+
+      subjectFtcVesting = await deployer.token.deployFtcVesting(
+        index.address,
+        recipient.address,
+        treasury.address,
+        subjectVestingAmount,
+        subjectVestingStart,
+        subjectVestingCliff,
+        subjectVestingEnd,
+      );
+    });
+
+    async function subject(): Promise<ContractTransaction> {
+      return subjectFtcVesting.connect(treasury.wallet).setTreasury(newTreasury.address);
+    }
+
+    it("should set new treasury address", async () => {
+      const currentTreasuryAddress = await subjectFtcVesting.treasury();
+
+      expect(currentTreasuryAddress).to.eq(treasury.address);
+
+      await subject();
+
+      const newTreasuryAddress = await subjectFtcVesting.treasury();
+
+      expect(newTreasuryAddress).to.eq(newTreasury.address);
     });
   });
 
@@ -130,7 +174,7 @@ describe("FTCVesting", () => {
       subjectVestingStart = now.sub(60 * 60 * 24 * 185);
       subjectVestingCliff = now;
       subjectVestingEnd = now.add(60 * 60 * 24 * 546);
-      subjectVestingAmount = (await index.totalSupply()).div(10);
+      subjectVestingAmount = ether(1);
 
       subjectFtcVesting = await deployer.token.deployFtcVesting(
         index.address,
@@ -141,20 +185,76 @@ describe("FTCVesting", () => {
         subjectVestingCliff,
         subjectVestingEnd,
       );
+
+      await index.transfer(subjectFtcVesting.address, ether(1000));
     });
 
     async function subject(): Promise<ContractTransaction> {
       return await subjectFtcVesting.connect(recipient.wallet).claim();
     }
 
-    it("should set new recipient", async () => {
-      const beforeIndexAmount = await index.connect(recipient.wallet).balanceOf(recipient.address);
+    it("should make a claim", async () => {
+      const unclaimedAmount = await index.connect(recipient.wallet).balanceOf(recipient.address);
+      const preClaimBalance = await index.balanceOf(subjectFtcVesting.address);
+      await subject();
+      const claimedAmount = await index.connect(recipient.wallet).balanceOf(recipient.address);
+      const postClaimBalance = await index.balanceOf(subjectFtcVesting.address);
 
+      const hasClaimed = claimedAmount.gt(unclaimedAmount);
+      const hasLowerBalance = preClaimBalance.gt(postClaimBalance);
+
+      expect(hasClaimed).to.be.true;
+      expect(hasLowerBalance).to.be.true;
+    });
+  });
+
+  describe("#clawback", async () => {
+    let subjectVestingAmount: BigNumber;
+    let subjectVestingStart: BigNumber;
+    let subjectVestingCliff: BigNumber;
+    let subjectVestingEnd: BigNumber;
+    let subjectFtcVesting: FTCVesting;
+
+    beforeEach(async () => {
+      const now = await getLastBlockTimestamp();
+      subjectVestingStart = now.sub(60 * 60 * 24 * 185);
+      subjectVestingCliff = now;
+      subjectVestingEnd = now.add(60 * 60 * 24 * 546);
+      subjectVestingAmount = ether(1);
+
+      subjectFtcVesting = await deployer.token.deployFtcVesting(
+        index.address,
+        recipient.address,
+        treasury.address,
+        subjectVestingAmount,
+        subjectVestingStart,
+        subjectVestingCliff,
+        subjectVestingEnd,
+      );
+
+      await index.transfer(subjectFtcVesting.address, ether(1000));
+    });
+
+    async function subject(): Promise<ContractTransaction> {
+      return await subjectFtcVesting.connect(treasury.wallet).clawback();
+    }
+
+    it("should clawback funds", async () => {
+      const unclaimedAmount = await index.connect(treasury.wallet).balanceOf(treasury.address);
       await subject();
 
-      const afterIndexAmount = await index.connect(recipient.wallet).balanceOf(recipient.address);
+      const claimedAmount = await index.connect(treasury.wallet).balanceOf(treasury.address);
+      const hasClaimed = claimedAmount.gt(unclaimedAmount);
 
-      expect(beforeIndexAmount).to.be.lessThan(afterIndexAmount);
+      expect(hasClaimed).to.be.true;
+    });
+
+    it("should have no funds remaining in the contract", async () => {
+      await subject();
+
+      const remainingIndex = await index.balanceOf(subjectFtcVesting.address);
+
+      expect(remainingIndex.isZero()).to.be.true;
     });
   });
 });
