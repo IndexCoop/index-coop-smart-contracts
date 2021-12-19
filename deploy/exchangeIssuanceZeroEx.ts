d59a731667ebe0f65284e16f1d4c3c4a6a27e980
Christian Koopmann <christian@MTMAC0057.local>
2021-12-19 11:26:52 +0100

    Adjust deploy script to approve set token

--- deploy/exchangeIssuanceZeroEx.ts
+++ deploy/exchangeIssuanceZeroEx.ts
@@ -1,19 +1,17 @@
 import { DeployFunction } from "hardhat-deploy/types";
 
-const func: DeployFunction = async function({ getNamedAccounts, deployments }) {
-  const { deployer } = await getNamedAccounts();
-  const { deploy } = deployments;
+const func: DeployFunction = async function({ ethers }) {
+  const ExchangeIssuanceZeroEx = await ethers.getContractFactory("ExchangeIssuanceZeroEx");
   // Mainnet addresses
   const wethAddress = "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2";
   const controllerAddress = "0xa4c8d221d8bb851f83aadd0223a8900a6921a349";
   const issuanceModuleAddress = "0xd8EF3cACe8b4907117a45B0b125c68560532F94D";
   const zeroExProxyAddress = "0xDef1C0ded9bec7F1a1670819833240f027b25EfF";
 
-  const exchangeIssuanceZeroEx = await deploy("ExchangeIssuanceZeroEx", {
-    from: deployer,
-    args: [wethAddress, controllerAddress, issuanceModuleAddress, zeroExProxyAddress],
-    log: true,
-  });
-  await exchangeIssuanceZeroEx.approveSetToken("");
+  const dpiAddress = "0x1494ca1f11d487c2bbe4543e90080aeba4ba3c2b";
+
+  const exchangeIssuanceZeroEx = await ExchangeIssuanceZeroEx.deploy(wethAddress, controllerAddress, issuanceModuleAddress, zeroExProxyAddress)
+  console.log("Exchange Issuacne deployed to", exchangeIssuanceZeroEx.address);
+  await exchangeIssuanceZeroEx.approveSetToken(dpiAddress);
 };
 export default func;
