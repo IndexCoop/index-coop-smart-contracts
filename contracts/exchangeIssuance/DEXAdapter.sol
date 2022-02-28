5efb93ffb4ad1722bb95b10e9864b7a2cd832dcb
Christian Koopmann <christian@MTMAC0057.local>
2022-02-28 10:51:05 +0800

    Remove unneeded newPath variable

--- contracts/exchangeIssuance/DEXAdapter.sol
+++ contracts/exchangeIssuance/DEXAdapter.sol
@@ -264,10 +264,8 @@ abstract contract DEXAdapter {
 
     function _encodePathV3(address[] memory _path, uint24[] memory _fees) internal view returns (bytes memory path) {
         path = abi.encodePacked(_path[0]);
-        bytes memory newPath;
         for(uint i = 0; i < _fees.length; i++){
-            newPath = abi.encodePacked(_fees[i], _path[i+1]);
-            path = abi.encodePacked(path, newPath);
+            path = abi.encodePacked(path, _fees[i], _path[i+1]);
         }
     }
 
