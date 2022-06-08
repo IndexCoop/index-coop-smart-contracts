acd33b8aeb5d8dbe6630a5d49a2989408a281675
Christian Koopmann <christian@MTMAC0057.local>
2022-06-08 11:21:34 +1000

    Adjust test token to contain different maturities

--- test/exchangeIssuance/exchangeIssuanceNotional.spec.ts
+++ test/exchangeIssuance/exchangeIssuanceNotional.spec.ts
@@ -86,42 +86,50 @@ describe("ExchangeIssuanceNotional", () => {
           await assetToken.mint(ether(100));
         });
 
-        describe("When wrappedFCashMock is deployed", () => {
-          let wrappedfCashMock: WrappedfCashMock;
+        describe("When wrappedFCashMocks are deployed", () => {
+          let wrappedfCashMocks: Array<WrappedfCashMock>;
           let underlyingTokenBalance: BigNumber;
           let currencyId: number;
-          let maturity: number;
+          let maturities: Array<number>;
           beforeEach(async () => {
             const underlyingAddress =
               underlyingToken.address == setup.weth.address ? ETH_ADDRESS : underlyingToken.address;
-
-            wrappedfCashMock = await deployer.mocks.deployWrappedfCashMock(
-              assetToken.address,
-              underlyingAddress,
-              setup.weth.address,
-            );
-
             currencyId = 1;
-            maturity = (await ethers.provider.getBlock("latest")).timestamp + 30 * 24 * 3600;
+            maturities = [30, 90];
+            wrappedfCashMocks = [];
 
-            await wrappedfCashMock.initialize(currencyId, maturity);
+            for (const maturityDays of maturities) {
+              console.log("Adding maturity", maturityDays);
+              const wrappedfCashMock = await deployer.mocks.deployWrappedfCashMock(
+                assetToken.address,
+                underlyingAddress,
+                setup.weth.address,
+              );
 
-            await wrappedfCashFactoryMock.registerWrapper(
-              currencyId,
-              maturity,
-              wrappedfCashMock.address,
-            );
+              const maturity =
+                (await ethers.provider.getBlock("latest")).timestamp + maturityDays * 24 * 3600;
 
-            underlyingTokenBalance = ether(100);
-            await underlyingToken.transfer(owner.address, underlyingTokenBalance);
-            await underlyingToken.approve(wrappedfCashMock.address, underlyingTokenBalance);
+              await wrappedfCashMock.initialize(currencyId, maturity);
 
-            await wrappedfCashMock.mintViaUnderlying(
-              underlyingTokenBalance,
-              underlyingTokenBalance,
-              owner.address,
-              0,
-            );
+              await wrappedfCashFactoryMock.registerWrapper(
+                currencyId,
+                maturity,
+                wrappedfCashMock.address,
+              );
+
+              underlyingTokenBalance = ether(100);
+              await underlyingToken.transfer(owner.address, underlyingTokenBalance);
+              await underlyingToken.approve(wrappedfCashMock.address, underlyingTokenBalance);
+
+              await wrappedfCashMock.mintViaUnderlying(
+                underlyingTokenBalance,
+                underlyingTokenBalance,
+                owner.address,
+                0,
+              );
+              wrappedfCashMocks.push(wrappedfCashMock);
+              console.log("Added maturity");
+            }
           });
           describe("When setToken is deployed", () => {
             let fCashPosition: BigNumber;
@@ -131,8 +139,8 @@ describe("ExchangeIssuanceNotional", () => {
               fCashPosition = ethers.utils.parseUnits("2", 9);
 
               setToken = await setup.createSetToken(
-                [wrappedfCashMock.address],
-                [fCashPosition],
+                wrappedfCashMocks.map(mock => mock.address),
+                wrappedfCashMocks.map(() => fCashPosition),
                 [debtIssuanceModule.address],
                 manager.address,
               );
@@ -151,9 +159,21 @@ describe("ExchangeIssuanceNotional", () => {
 
               initialSetBalance = underlyingTokenBalance.div(10);
 
-              await wrappedfCashMock.mintViaUnderlying(0, underlyingTokenBalance, owner.address, 0);
-              await wrappedfCashMock.approve(debtIssuanceModule.address, underlyingTokenBalance);
+              for (const wrappedfCashMock of wrappedfCashMocks) {
+                await wrappedfCashMock.mintViaUnderlying(
+                  0,
+                  underlyingTokenBalance,
+                  owner.address,
+                  0,
+                );
+                await wrappedfCashMock.approve(
+                  debtIssuanceModule.address,
+                  ethers.constants.MaxUint256,
+                );
+              }
+              console.log("Issuing some set");
               await debtIssuanceModule.issue(setToken.address, initialSetBalance, owner.address);
+              console.log("Issued some set");
             });
 
             describe("When exchangeIssuance is deployed", () => {
@@ -218,6 +238,11 @@ describe("ExchangeIssuanceNotional", () => {
                         subjectMaxAmountInputToken = (
                           await inputToken.balanceOf(caller.address)
                         ).div(10);
+                        for (const wrappedfCashMock of wrappedfCashMocks) {
+                          await wrappedfCashMock.setMintTokenSpent(
+                            subjectMaxAmountInputToken.div(wrappedfCashMocks.length),
+                          );
+                        }
                         expect(subjectMaxAmountInputToken).to.be.gt(0);
                       });
 
@@ -249,7 +274,6 @@ describe("ExchangeIssuanceNotional", () => {
                   let subjectIssuanceModule: Address;
                   let subjectIsDebtIssuance: boolean;
                   let caller: Account;
-
                   beforeEach(async () => {
                     subjectSetToken = setToken.address;
                     subjectSetAmount = ethers.utils.parseEther("1");
@@ -258,7 +282,6 @@ describe("ExchangeIssuanceNotional", () => {
                     subjectMinAmountOutputToken = BigNumber.from(0);
                     caller = owner;
                   });
-
                   function subject() {
                     return exchangeIssuance
                       .connect(caller.wallet)
@@ -297,10 +320,14 @@ describe("ExchangeIssuanceNotional", () => {
                           subjectOutputToken = outputToken.address;
                           redeemAmountReturned = BigNumber.from(1000);
                           subjectMinAmountOutputToken = redeemAmountReturned;
-                          await wrappedfCashMock.setRedeemTokenReturned(redeemAmountReturned);
-                          await outputToken.transfer(wrappedfCashMock.address, redeemAmountReturned);
+                          wrappedfCashMocks.forEach(async wrappedfCashMock => {
+                            await wrappedfCashMock.setRedeemTokenReturned(redeemAmountReturned);
+                            await outputToken.transfer(
+                              wrappedfCashMock.address,
+                              redeemAmountReturned,
+                            );
+                          });
                         });
-
                         it("should redeem correct amount of set token", async () => {
                           const balanceBefore = await setToken.balanceOf(caller.address);
                           await subject();
@@ -309,7 +336,6 @@ describe("ExchangeIssuanceNotional", () => {
                           );
                           expect(redeemedAmount).to.eq(subjectSetAmount);
                         });
-
                         it("should return correct amount of output token", async () => {
                           const balanceBefore = await outputToken.balanceOf(caller.address);
                           await subject();
