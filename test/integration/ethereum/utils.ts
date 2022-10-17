d62a67da39ee5c25e873fc328c7b61f2a72ae649
ckoopmann <c.k.e.koopmann@gmail.com>
2022-10-17 14:17:30 +0300

    Remove unneeded dependencies

--- test/integration/ethereum/utils.ts
+++ test/integration/ethereum/utils.ts
@@ -1,11 +1,10 @@
 import { ethers, network } from "hardhat";
-import { BigNumber, Signer } from "ethers";
+import { BigNumber } from "ethers";
 import { INotionalProxy, IWrappedfCashComplete, IWrappedfCashFactory } from "@typechain/index";
 import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/dist/src/signer-with-address";
 import { IERC20 } from "@typechain/IERC20";
 import { ICErc20 } from "@typechain/ICErc20";
 
-const NEW_ROUTER_ADDRESS = "0x16eD130F7A6dcAc7e3B0617A7bafa4b470189962";
 const NOTIONAL_PROXY_ADDRESS = "0x1344A36A1B56144C3Bc62E7757377D288fDE0369";
 
 const cEthAddress = "0x4Ddc2D193948926D02f9B1fE9e1daa0718270ED5";
@@ -18,32 +17,6 @@ export async function impersonateAccount(address: string) {
   return ethers.provider.getSigner(address);
 }
 
-export async function upgradeNotionalProxy(signer: Signer) {
-  // This is the notional contract w/ notional abi
-  const notional = (await ethers.getContractAt(
-    "INotionalProxy",
-    NOTIONAL_PROXY_ADDRESS,
-  )) as INotionalProxy;
-
-  const notionalOwner = await impersonateAccount(await notional.owner());
-
-  const fundingValue = ethers.utils.parseEther("10");
-  await signer.sendTransaction({ to: await notionalOwner.getAddress(), value: fundingValue });
-
-  await notional.connect(notionalOwner).upgradeTo(NEW_ROUTER_ADDRESS);
-  await notional
-    .connect(notionalOwner)
-    .updateAssetRate(1, "0x8E3D447eBE244db6D28E2303bCa86Ef3033CFAd6");
-  await notional
-    .connect(notionalOwner)
-    .updateAssetRate(2, "0x719993E82974f5b5eA0c5ebA25c260CD5AF78E00");
-  await notional
-    .connect(notionalOwner)
-    .updateAssetRate(3, "0x612741825ACedC6F88D8709319fe65bCB015C693");
-  await notional
-    .connect(notionalOwner)
-    .updateAssetRate(4, "0x39D9590721331B13C8e9A42941a2B961B513E69d");
-}
 
 export async function getCurrencyIdAndMaturity(underlyingAddress: string, maturityIndex: number) {
   const notionalProxy = (await ethers.getContractAt(
