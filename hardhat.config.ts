ff429ea9b8fe7e1baa7ecb52576e55b253373bb8
ckoopmann <c.k.e.koopmann@gmail.com>
2022-10-17 14:19:33 +0300

    Remove unneeded v8 solidity from hardhat config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -61,15 +61,6 @@ const config: HardhatUserConfig = {
         version: "0.6.10",
         settings: { optimizer: { enabled: true, runs: 200 } },
       },
-      {
-        version: "0.8.11",
-        settings: {
-          optimizer: {
-            enabled: true,
-            runs: 200,
-          },
-        },
-      },
     ],
   },
   namedAccounts: {
