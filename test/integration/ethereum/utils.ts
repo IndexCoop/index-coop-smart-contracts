2d0fb988dc71dae86867dbb4f37daf8418b4f766
christn <c.k.e.koopmann@gmail.com>
2022-10-20 09:16:51 +0300

    Remove empty if branch
    Co-authored-by: SnakePoison <ElmoArmy@users.noreply.github.com>

--- test/integration/ethereum/utils.ts
+++ test/integration/ethereum/utils.ts
@@ -66,10 +66,7 @@ export async function mintWrappedFCash(
     depositAmountExternal = underlyingTokenAmount;
   } else {
     const assetTokenBalanceBefore = await assetToken.balanceOf(signer.address);
-    if (assetToken.address == cEthAddress) {
-      // assetToken = assetToken as ICEth;
-      // await assetToken.connect(signer).mint({ value: underlyingTokenAmount });
-    } else {
+    if (assetToken.address != cEthAddress) {   
       assetToken = assetToken as ICErc20;
       await underlyingToken.connect(signer).approve(assetToken.address, underlyingTokenAmount);
       await assetToken.connect(signer).mint(underlyingTokenAmount);
