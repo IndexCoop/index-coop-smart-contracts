ed0e5b4a083ac6f0e2fcf4cb880e15bddb2148f6
Richard Guan <guan.richard1@gmail.com>
2022-11-15 12:18:12 +1100

    add trade module

--- contracts/interfaces/ITradeModule.sol
+++ contracts/interfaces/ITradeModule.sol
@@ -19,6 +19,15 @@ pragma solidity 0.6.10;
 import { ISetToken } from "../interfaces/ISetToken.sol";
 
 interface ITradeModule {
+    function initialize(ISetToken _setToken) external;
 
-    function trade(ISetToken _setToken) external;    
+    function trade(
+        ISetToken _setToken,
+        string memory _exchangeName,
+        address _sendToken,
+        uint256 _sendQuantity,
+        address _receiveToken,
+        uint256 _minReceiveQuantity,
+        bytes memory _data
+    ) external;
 }
