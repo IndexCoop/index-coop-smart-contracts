443f507a8cba15ad8dec89f100a68cb778ec3e13
Richard Guan <guan.richard1@gmail.com>
2022-11-15 12:33:36 +1100

    add initial logic

--- contracts/adapters/StakeWiseReinvestmentExtension.sol
+++ contracts/adapters/StakeWiseReinvestmentExtension.sol
@@ -20,6 +20,8 @@ pragma solidity 0.6.10;
 pragma experimental ABIEncoderV2;
 
 import { BaseExtension } from "../lib/BaseExtension.sol";
+import { IBaseManager } from "../interfaces/IBaseManager.sol";
+import { ISetToken } from "../interfaces/ISetToken.sol";
 import { IAirdropModule } from "../interfaces/IAirdropModule.sol";
 import { ITradeModule } from "../interfaces/ITradeModule.sol";
 
@@ -31,9 +33,37 @@ import { ITradeModule } from "../interfaces/ITradeModule.sol";
  */
 contract StakeWiseReinvestmentExtension is BaseExtension {
 
+    ISetToken setToken;
     IAirdropModule airdropModule;
+    ITradeModule tradeModule;
 
-    function reinvest() {
+    constructor(
+        IBaseManager _manager,
+        IAirdropModule _airdropModule,
+        ITradeModule _tradeModule
+    ) public BaseExtension(_manager) {
+        setToken = _manager.setToken();
+        airdropModule = _airdropModule;
+        tradeModule = _tradeModule;
+    }
 
+    function reinvest(address memory _exchangeName, uint256 _minReceiveQuantity) external onlyAllowedCaller(msg.sender) {
+        bytes memory absorbCallData = abi.encodeWithSelector(
+            IAirdropModule.absorb.selector,
+            setToken,
+            "rETH2"
+        );
+        invokeManager(airdropModule, absorbCallData);
+
+        bytes memory tradeCallData = abi.encodeWithSelector(
+            ITradeModule.trade.selector,
+            _exchangeName,
+            "rETH2",
+            setToken.getTotalComponentRealUnits("rETH2"),
+            "sETH2",
+            _minReceiveQuantity,
+            "TODO: add trade callData"
+        );
+        invokeManager(tradeModule, tradeCallData);
     }
 }
