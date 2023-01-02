a68bff83b6010b59949450810ff3c2e27ca0b51d
Richard Guan <guan.richard1@gmail.com>
2023-01-02 12:10:30 +1100

    update copyright

--- contracts/viewers/SetTokenRateViewer.sol
+++ contracts/viewers/SetTokenRateViewer.sol
@@ -1,5 +1,5 @@
 /*
-    Copyright 2022 IndexCoop.
+    Copyright 2023 IndexCoop.
 
     Licensed under the Apache License, Version 2.0 (the "License");
     you may not use this file except in compliance with the License.
