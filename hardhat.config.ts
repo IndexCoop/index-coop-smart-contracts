56c08f4b88f9f03914f3806c6248ede06755dac5
Richard Guan <guan.richard1@gmail.com>
2023-03-28 17:10:23 +0900

    parallel

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -36,7 +36,6 @@ const forkingConfig =
 
 const mochaConfig = {
   timeout: process.env.INTEGRATIONTEST ? INTEGRATIONTEST_TIMEOUT : 50000,
-  parallel: !process.env.INTEGRATIONTEST,
 } as Mocha.MochaOptions;
 
 const gasOption =
