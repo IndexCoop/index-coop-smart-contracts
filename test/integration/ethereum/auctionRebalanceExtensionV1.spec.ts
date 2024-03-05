ce0193e3080b62505abcc528fa20d00585eee448
Pranav Bhardwaj <pblivin0x@gmail.com>
2024-03-05 09:27:10 -0500

    update tests

--- test/integration/ethereum/auctionRebalanceExtensionV1.spec.ts
+++ test/integration/ethereum/auctionRebalanceExtensionV1.spec.ts
@@ -2,7 +2,7 @@ import "module-alias/register";
 
 import { Address, Account } from "@utils/types";
 import { setBlockNumber } from "@utils/test/testingUtils";
-import { ZERO } from "@utils/constants";
+import { MAX_UINT_256, ZERO } from "@utils/constants";
 import {
   AuctionRebalanceModuleV1,
   AuctionRebalanceModuleV1__factory,
@@ -92,7 +92,7 @@ if (process.env.INTEGRATIONTEST) {
         let subjectPositionMultiplier: BigNumber;
         let subjectCaller: Signer;
 
-        beforeEach(async () => {
+        before(async () => {
           // Quote asset is WETH, which has 18 decimals
           // Auction components may have different decimals, which must be adjusted for in price
           subjectQuoteAsset = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2"; // WETH
@@ -113,77 +113,89 @@ if (process.env.INTEGRATIONTEST) {
           ];
 
           // Components to add to the SetToken during the rebalance
-          subjectNewComponents = [
-            "0x514910771AF9Ca656af840dff83E8264,EcF986CA", // LINK
-          ];
+          subjectNewComponents = [];
+          // subjectNewComponents = [
+          //   "0x514910771AF9Ca656af840dff83E8264,EcF986CA", // LINK
+          // ];
 
           // Must match order of subjectNewComponents
-          subjectNewComponentsAuctionParams = [
-            { // LINK, 18 decimals
-                targetUnit: "100000000000000000",
-                priceAdapterName: "ConstantPriceAdapter",
-                priceAdapterConfigData: await priceAdapter.getEncodedData(ether(0.007468)),
-              },
-          ];
+          subjectNewComponentsAuctionParams = [];
+          // subjectNewComponentsAuctionParams = [
+          //   { // LINK, 18 decimals
+          //       targetUnit: "100000000000000000",
+          //       priceAdapterName: "ConstantPriceAdapter",
+          //       priceAdapterConfigData: await priceAdapter.getEncodedData(ether(0.007468)),
+          //     },
+          // ];
 
           // Must match order of subjectOldComponents
           subjectOldComponentsAuctionParams = [
             { // 21BTC, 8 decimals
-                targetUnit: "150000",
-                priceAdapterName: "ConstantPriceAdapter",
-                priceAdapterConfigData: await priceAdapter.getEncodedData(ether(18.52).mul(ether(1).div(1e8))),
-              },
-              { // WETH, 18 decimals, this price curve won't be used because it's the quote asset
-                targetUnit: "1000000000000000000",
-                priceAdapterName: "ConstantPriceAdapter",
-                priceAdapterConfigData: await priceAdapter.getEncodedData(ether(1)),
-              },
-              { // 21BNB, 8 decimals
-                targetUnit: "2013293",
-                priceAdapterName: "ConstantPriceAdapter",
-                priceAdapterConfigData: await priceAdapter.getEncodedData(ether(0.13).mul(ether(1).div(1e8))),
-              },
-              { // 21XRP, 6 decimals
-                targetUnit: "10000000",
-                priceAdapterName: "ConstantPriceAdapter",
-                priceAdapterConfigData: await priceAdapter.getEncodedData(ether(1).mul(ether(0.00022).div(1e6))),
-              },
-              { // 21ADA, 6 decimals
-                targetUnit: "10639431",
-                priceAdapterName: "ConstantPriceAdapter",
-                priceAdapterConfigData: await priceAdapter.getEncodedData(ether(1).mul(ether(0.0002).div(1e6))),
-              },
-              { // 21SOL, 9 decimals
-                targetUnit: "111427278",
-                priceAdapterName: "ConstantPriceAdapter",
-                priceAdapterConfigData: await priceAdapter.getEncodedData(ether(0.04).mul(ether(1).div(1e9))),
-              },
-              { // 21LTC, 8 decimals
-                targetUnit: ZERO, // Remove from the set
-                priceAdapterName: "ConstantPriceAdapter",
-                priceAdapterConfigData: await priceAdapter.getEncodedData(ether(0.03).mul(ether(1).div(1e8))),
-              },
-              { // 21DOT, 10 decimals
-                targetUnit: "5134697410",
-                priceAdapterName: "ConstantPriceAdapter",
-                priceAdapterConfigData: await priceAdapter.getEncodedData(ether(0.0029).mul(ether(1).div(1e10))),
-              },
-              { // MATIC, 18 decimals
-                targetUnit: "5000000000000000000",
-                priceAdapterName: "ConstantPriceAdapter",
-                priceAdapterConfigData: await priceAdapter.getEncodedData(ether(0.0003457)),
-              },
-              { // 21BCH, 8 decimals
-                targetUnit: ZERO, // Remove from the set
-                priceAdapterName: "ConstantPriceAdapter",
-                priceAdapterConfigData: await priceAdapter.getEncodedData(ether(0.1).mul(ether(1).div(1e8))),
-              },
+              targetUnit: "128270",
+              priceAdapterName: "ConstantPriceAdapter",
+              priceAdapterConfigData: await priceAdapter.getEncodedData(ether(17.972862).mul(ether(1).div(1e8))),
+            },
+            { // WETH, 18 decimals, this price curve won't be used because it's the quote asset
+              targetUnit: "12760000000000000",
+              priceAdapterName: "ConstantPriceAdapter",
+              priceAdapterConfigData: await priceAdapter.getEncodedData(ether(1)),
+            },
+            { // 21BNB, 8 decimals
+              targetUnit: "4078225",
+              priceAdapterName: "ConstantPriceAdapter",
+              priceAdapterConfigData: await priceAdapter.getEncodedData(ether(0.11187201).mul(ether(1).div(1e8))),
+            },
+            { // 21XRP, 6 decimals
+              targetUnit: "22272822",
+              priceAdapterName: "ConstantPriceAdapter",
+              priceAdapterConfigData: await priceAdapter.getEncodedData(ether(0.00017329).mul(ether(1).div(1e6))),
+            },
+            { // 21ADA, 6 decimals
+              targetUnit: "16274778",
+              priceAdapterName: "ConstantPriceAdapter",
+              priceAdapterConfigData: await priceAdapter.getEncodedData(ether(0.00020616).mul(ether(1).div(1e6))),
+            },
+            { // 21SOL, 9 decimals
+              targetUnit: "140580280",
+              priceAdapterName: "ConstantPriceAdapter",
+              priceAdapterConfigData: await priceAdapter.getEncodedData(ether(0.03513891).mul(ether(1).div(1e9))),
+            },
+            { // 21LTC, 8 decimals
+              targetUnit: ZERO, // Remove from the set
+              priceAdapterName: "ConstantPriceAdapter",
+              priceAdapterConfigData: await priceAdapter.getEncodedData(ether(0.02399340).mul(ether(1).div(1e8))),
+            },
+            { // 21DOT, 10 decimals
+              targetUnit: "8783960596",
+              priceAdapterName: "ConstantPriceAdapter",
+              priceAdapterConfigData: await priceAdapter.getEncodedData(ether(0.00277256).mul(ether(1).div(1e10))),
+            },
+            { // MATIC, 18 decimals
+              targetUnit: "6575386581000000000",
+              priceAdapterName: "ConstantPriceAdapter",
+              priceAdapterConfigData: await priceAdapter.getEncodedData(ether(0.00030279)),
+            },
+            { // 21BCH, 8 decimals
+              targetUnit: ZERO, // Remove from the set
+              priceAdapterName: "ConstantPriceAdapter",
+              priceAdapterConfigData: await priceAdapter.getEncodedData(ether(0.12018696).mul(ether(1).div(1e8))),
+            },
           ];
 
-          subjectShouldLockSetToken = true; // pause issuance and redemption during the rebalance
-          subjectRebalanceDuration = BigNumber.from(86400); // 1 day in seconds
+          subjectShouldLockSetToken = false; // pause issuance and redemption during the rebalance
+          subjectRebalanceDuration = BigNumber.from(86400); // 1 hour in seconds
           subjectPositionMultiplier = await ic21.positionMultiplier(); // grab the current position multiplier
           subjectCaller = operator;
+
+          console.log("Parameters for startRebalance:");
+          console.log("subjectQuoteAsset", subjectQuoteAsset);
+          console.log("subjectOldComponents", subjectOldComponents);
+          console.log("subjectNewComponents", subjectNewComponents);
+          console.log("subjectNewComponentsAuctionParams", subjectNewComponentsAuctionParams);
+          console.log("subjectOldComponentsAuctionParams", subjectOldComponentsAuctionParams);
+          console.log("subjectShouldLockSetToken", subjectShouldLockSetToken);
+          console.log("subjectRebalanceDuration", subjectRebalanceDuration.toString());
+          console.log("subjectPositionMultiplier", subjectPositionMultiplier.toString());
         });
 
         async function subject(): Promise<ContractTransaction> {
@@ -207,6 +219,158 @@ if (process.env.INTEGRATIONTEST) {
 
           const isElapsed = await auctionModule.isRebalanceDurationElapsed(ic21.address);
           expect(isElapsed).to.be.false;
+        });
+
+        it("should set the correct 21BTC market", async () => {
+          await subject();
+
+          const preview = await auctionModule.getBidPreview(
+            ic21.address,
+            subjectOldComponents[0],
+            subjectQuoteAsset,
+            MAX_UINT_256,
+            MAX_UINT_256,
+            true
+          );
+
+          // 0.12408063 BTC <> 2.23008403986306 WETH
+          expect(preview.receiveToken).to.eq(subjectQuoteAsset);
+          expect(preview.sendToken).to.eq(subjectOldComponents[0]);
+          expect(preview.quantitySentBySet).to.eq("12408063");
+          expect(preview.quantityReceivedBySet).to.eq("2230084039863060000");
+        });
+
+        it("should set the correct 21BNB market", async () => {
+          await subject();
+
+          const preview = await auctionModule.getBidPreview(
+            ic21.address,
+            subjectOldComponents[2],
+            subjectQuoteAsset,
+            MAX_UINT_256,
+            0,
+            false
+          );
+
+          // 0.19108137796 ETH <> 1.70803562 21BNB
+          expect(preview.sendToken).to.eq(subjectQuoteAsset);
+          expect(preview.receiveToken).to.eq(subjectOldComponents[2]);
+          expect(preview.quantitySentBySet).to.eq("191081377960996200");
+          expect(preview.quantityReceivedBySet).to.eq("170803562");
+        });
+
+        it("should set the correct 21XRP market", async () => {
+          await subject();
+
+          const preview = await auctionModule.getBidPreview(
+            ic21.address,
+            subjectOldComponents[3],
+            subjectQuoteAsset,
+            MAX_UINT_256,
+            0,
+            false
+          );
+
+          // 2.89349384341 WETH <> 16697.408064 XRP
+          expect(preview.sendToken).to.eq(subjectQuoteAsset);
+          expect(preview.receiveToken).to.eq(subjectOldComponents[3]);
+          expect(preview.quantitySentBySet).to.eq("2893493843410560000");
+          expect(preview.quantityReceivedBySet).to.eq("16697408064");
+        });
+
+        it("should set the correct 21ADA market", async () => {
+          await subject();
+
+          const preview = await auctionModule.getBidPreview(
+            ic21.address,
+            subjectOldComponents[4],
+            subjectQuoteAsset,
+            MAX_UINT_256,
+            MAX_UINT_256,
+            true
+          );
+
+          // 0.74005633524 WETH <> 3589.718351 21ADA
+          expect(preview.receiveToken).to.eq(subjectQuoteAsset);
+          expect(preview.sendToken).to.eq(subjectOldComponents[4]);
+          expect(preview.quantitySentBySet).to.eq("3589718351");
+          expect(preview.quantityReceivedBySet).to.eq("740056335242160000");
+        });
+
+        it("should set the correct 21SOL market", async () => {
+          await subject();
+
+          const preview = await auctionModule.getBidPreview(
+            ic21.address,
+            subjectOldComponents[5],
+            subjectQuoteAsset,
+            MAX_UINT_256,
+            MAX_UINT_256,
+            true
+          );
+
+          // 6.5485911871 WETH <> 186.362957391 21SOL
+          expect(preview.receiveToken).to.eq(subjectQuoteAsset);
+          expect(preview.sendToken).to.eq(subjectOldComponents[5]);
+          expect(preview.quantitySentBySet).to.eq("186362957391");
+          expect(preview.quantityReceivedBySet).to.eq("6548591187096183810");
+        });
+
+        it("should set the correct 21LTC market", async () => {
+          await subject();
+
+          const preview = await auctionModule.getBidPreview(
+            ic21.address,
+            subjectOldComponents[6],
+            subjectQuoteAsset,
+            MAX_UINT_256,
+            MAX_UINT_256,
+            true
+          );
+
+          // 3.66166316692 WETH <> 152.61126672 21LTC
+          expect(preview.receiveToken).to.eq(subjectQuoteAsset);
+          expect(preview.sendToken).to.eq(subjectOldComponents[6]);
+          expect(preview.quantitySentBySet).to.eq("15261126672");
+          expect(preview.quantityReceivedBySet).to.eq("3661663166919648000");
+        });
+
+        it("should set the correct 21DOT market", async () => {
+          await subject();
+
+          const preview = await auctionModule.getBidPreview(
+            ic21.address,
+            subjectOldComponents[7],
+            subjectQuoteAsset,
+            MAX_UINT_256,
+            0,
+            false
+          );
+
+          // 0.47352168559 WETH <> 170.788616151 21DOT
+          expect(preview.sendToken).to.eq(subjectQuoteAsset);
+          expect(preview.receiveToken).to.eq(subjectOldComponents[7]);
+          expect(preview.quantitySentBySet).to.eq("473521685595893816");
+          expect(preview.quantityReceivedBySet).to.eq("1707886161511");
+        });
+
+        it("should set the correct 21BCH market", async () => {
+          await subject();
+
+          const preview = await auctionModule.getBidPreview(
+            ic21.address,
+            subjectOldComponents[9],
+            subjectQuoteAsset,
+            MAX_UINT_256,
+            MAX_UINT_256,
+            true
+          );
+
+          // 4.65454307982 WETH <> 38.72752152 21BCH
+          expect(preview.receiveToken).to.eq(subjectQuoteAsset);
+          expect(preview.sendToken).to.eq(subjectOldComponents[9]);
+          expect(preview.quantitySentBySet).to.eq("3872752152");
+          expect(preview.quantityReceivedBySet).to.eq("4654543079823379200");
         });
       });
   });
