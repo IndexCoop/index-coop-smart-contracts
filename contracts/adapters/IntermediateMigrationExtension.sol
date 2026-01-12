7395b6b47edeb6a6cae76fd93ca0b8ee19a94384
christn <c.k.e.koopmann@gmail.com>
2026-01-12 13:50:23 +0100

    chore: Update year in copyright section

--- contracts/adapters/IntermediateMigrationExtension.sol
+++ contracts/adapters/IntermediateMigrationExtension.sol
@@ -1,5 +1,5 @@
 /*
-    Copyright 2024 Index Coop
+    Copyright 2026 Index Coop
 
     Licensed under the Apache License, Version 2.0 (the "License");
     you may not use this file except in compliance with the License.
